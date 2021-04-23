fb532839ddb8eacee966cd22b644a39539782bd3
mrpickles-dev <mr.pickles300@gmail.com>
2021-04-23 18:51:38 +0200

    final update

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 
-
-
-
 // main component
 
 class App extends React.Component {
@@ -11,79 +8,165 @@ class App extends React.Component {
         sessionLength: 25,
         sessionBreak: 5,
         sessionDisplay:'Session',
-        time: 1500
+        paused: true,
+        time: 1500,
+        active: undefined
       };
     
-  
-      this.handleClick = this.handleClick.bind(this);
-      this.handleIncrement = this.handleIncrement.bind(this);
-      this.minSecConverter = this.minSecConverter.bind(this);
-
-      }  
-    
-
-   
-    handleClick(){
-        const { time } = this.state;
-        console.log(time)
-if (time > 0){
-    setInterval( this.handleIncrement() 
-        , 1000);
-} else
-return console.log("Stopped")
-
-/*     for (let i = time; i > 0; i--){
-         setInterval( this.handleIncrement() 
-        , 1000);
-    }
-/*  */
-    }
+    }  
+     
+    // Convert time 
 
  minSecConverter() {
         let minutes = Math.floor(this.state.time / 60);
         let seconds = this.state.time - minutes * 60;
+
         seconds = seconds < 10 ? '0' + seconds : seconds;
         minutes = minutes < 10 ? '0' + minutes : minutes;
+
         return minutes + ':' + seconds;
       }
 
+// Reset the timer
 
+      resetTime = () => {
+        const audio = document.getElementById("beep");
+        audio.pause();
+        audio.currentTime = 0;
+        clearInterval(this.active);
+        this.setState({ 
+          time: 1500, 
+          sessionDisplay: 'Session', 
+          paused: true, 
+          sessionBreak: 5, 
+          sessionLength: 25 
+        });
+    }
+    
+// Play and Pause function
 
+    startPause = () => {
+      const { active, paused, sessionDisplay } = this.state;
+      const audio = document.getElementById("beep");
+      
+      if(paused === false){
+          clearInterval(this.active);
+          this.setState({ paused: true })
+      } else
+            {
+             this.setState({ 
+               paused : false
+               });
+             this.active = setInterval(() => {
+                             const { time, sessionDisplay, sessionBreak, sessionLength } = this.state
+                               
+                             if(time <= 6){
+                                document.getElementById("time-left").style.color = "red";
+                             }
+                             if(time === 0){
+                                sessionDisplay === 'Session' ? this.setState({ sessionDisplay : 'Break', time : sessionBreak * 60 }) : this.setState({ sessionDisplay : 'Session', time : sessionLength * 60 });
+                             
+                                audio.play();
+                             } else {
+                                    this.setState({ time : time - 1 })
+                             }
+                                           }, 1000);
+            }
+    }
 
-    handleIncrement() {
-     /*    const [timeValue, spazio] = this.state.time;
-        console.log(timeValue) */
+// Increment or Decrement session length
 
-/* for (let i = timeValue; i > 0; i++){
-
-    setInterval(function(){ 
-        console.log("è passato un se")
+      lengthControl = e => {
+        let { paused, sessionDisplay, sessionBreak, sessionLength, time } = this.state;
         
-     }, 1000); */
-return this.setState({ time: this.state.time -1})
-}
-
-     
-
+        let targeted = e.target.id.replace('-decrement', '').replace('-increment','');
+        
+        if(paused){
+            switch(e.target.id){
+              case 'break-decrement' :
+                 sessionBreak > 1 ? 
+                 sessionBreak = sessionBreak - 1 :
+                 sessionBreak = sessionBreak; 
+                 break;
+              case 'break-increment' :
+                 sessionBreak <= 59 ?
+                  sessionBreak = sessionBreak + 1 :
+                   sessionBreak = sessionBreak; 
+                   break;
+              case 'session-decrement' :
+                 sessionLength > 1 ?
+                  sessionLength = sessionLength - 1 :
+                   sessionLength = sessionLength;
+                   break;
+              case 'session-increment' :
+                sessionLength <= 59 ?
+                 sessionLength = sessionLength + 1 :
+                  sessionLength = sessionLength;
+                   break;
+            }
+        
+            if(sessionDisplay.toLowerCase() === targeted){
+                if(targeted === 'session'){
+                   time = sessionLength * 60;
+                } else {
+                     time = sessionBreak * 60;
+                }
+            }
+            
+            this.setState ({
+                  sessionBreak: sessionBreak,
+                  sessionLength: sessionLength,
+                  time: time
+            });
+        }
+      }
         
     render() {
-        const {time} = this.state;
+      const { sessionBreak, sessionLength, time, paused, sessionDisplay  } = this.state;        
         
       return (
         <div className="container">
             <h1>Timer 25 + 5</h1>
-        <div className="numDisplay">
-          {this.minSecConverter()}
-        </div>
-        <div className="play" href="#" onClick={this.handleClick}>
-            <button>Play</button>
-        </div>
+            <div className="painel">
+                <ControlLength myId="break-label" myText="Break Length" 
+                buttonPlus="break-increment" buttonMinus="break-decrement" 
+                myTag="break-length" myCount={sessionBreak} myFunction={this.lengthControl} />
+                <ControlLength myId="session-label" myText="Session Length"
+                 buttonPlus="session-increment" buttonMinus="session-decrement" 
+                 myTag="session-length" myCount={sessionLength} myFunction={this.lengthControl} />
+                </div>
+                <audio id="beep" preload="auto" 
+                src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav" />
+               <div id="painel2" className="painel2">
+                 <div  id="timer-label" className="timer-label">{sessionDisplay}</div>
+        <div id="time-left" className="time-left">{this.minSecConverter(time)}</div>
+     
+        <div className="buttons control">
+                       <div id="start_stop"  className={ paused ? "fas fa-play btns" : "fas fa-pause btns" } onClick={this.startPause} />
+                       <div id="reset" className="fas fa-redo btns"  onClick={this.resetTime} />
+                    </div>  
         
-        
+        </div>
         </div>
       );
     }
   }
+
+  // Controller for session length
+
+  const ControlLength = (props) => {
+    return (
+          <div className="indicator">
+              <h3 id={props.myId}>{props.myText}</h3>
+              <div className="buttons">
+                <div id={props.buttonMinus} onClick={props.myFunction}  className="fas fa-arrow-down" />
+                <p id={props.myTag}>{props.myCount}</p>
+                <div id={props.buttonPlus} onClick={props.myFunction} className="fas fa-arrow-up" />
+              </div>
+          </div>
+          )
+  }
+
 
 
   ReactDOM.render(<App/>, document.getElementById('app'))
